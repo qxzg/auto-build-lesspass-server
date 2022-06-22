e00a6467f3cb98a6cf3cec4edbc468b6d559dec2
qxzg <30212669+qxzg@users.noreply.github.com>
2022-06-22 10:13:26 +0800

    恢复env控制

--- build-lesspass-server.sh
+++ build-lesspass-server.sh
@@ -14,13 +14,13 @@ sed -i 's/lesspass.local/127.0.0.1/g' containers/backend/lesspass/settings.py
 #sed -i 's/days=7/days=14/g' containers/backend/lesspass/settings.py
 #sed -i 's/minutes=15/minutes=60/g' containers/backend/lesspass/settings.py
 
-sed -i 's/\"EMAIL_USE_SSL\", default=False/\"EMAIL_USE_SSL\", default=True/g' containers/backend/lesspass/settings.py
-sed -i 's/\"EMAIL_SUBJECT_PREFIX\", \"\[LessPass\] \"/\"EMAIL_SUBJECT_PREFIX\", \"\[qxzg`s LessPass\] \"/g' containers/backend/lesspass/settings.py
-sed -i 's/default=25/default=465/g' containers/backend/lesspass/settings.py
-sed -i 's/contact@lesspass.com/'${DEFAULT_FROM_EMAIL}'/g' containers/backend/lesspass/settings.py
-sed -i 's/\"EMAIL_HOST\", \"localhost\"/\"EMAIL_HOST\", \"'${EMAIL_HOST}'\"/g' containers/backend/lesspass/settings.py
-sed -i 's/\"EMAIL_HOST_USER\", \"\"/\"EMAIL_HOST_USER\", \"'${EMAIL_HOST_USER}'\"/g' containers/backend/lesspass/settings.py
-sed -i 's/\"EMAIL_HOST_PASSWORD\", \"\"/\"EMAIL_HOST_PASSWORD\", \"'${EMAIL_HOST_PASSWORD}'\"/g' containers/backend/lesspass/settings.py
+#sed -i 's/\"EMAIL_USE_SSL\", default=False/\"EMAIL_USE_SSL\", default=True/g' containers/backend/lesspass/settings.py
+#sed -i 's/\"EMAIL_SUBJECT_PREFIX\", \"\[LessPass\] \"/\"EMAIL_SUBJECT_PREFIX\", \"\[qxzg`s LessPass\] \"/g' containers/backend/lesspass/settings.py
+#sed -i 's/default=25/default=465/g' containers/backend/lesspass/settings.py
+#sed -i 's/contact@lesspass.com/'${DEFAULT_FROM_EMAIL}'/g' containers/backend/lesspass/settings.py
+#sed -i 's/\"EMAIL_HOST\", \"localhost\"/\"EMAIL_HOST\", \"'${EMAIL_HOST}'\"/g' containers/backend/lesspass/settings.py
+#sed -i 's/\"EMAIL_HOST_USER\", \"\"/\"EMAIL_HOST_USER\", \"'${EMAIL_HOST_USER}'\"/g' containers/backend/lesspass/settings.py
+#sed -i 's/\"EMAIL_HOST_PASSWORD\", \"\"/\"EMAIL_HOST_PASSWORD\", \"'${EMAIL_HOST_PASSWORD}'\"/g' containers/backend/lesspass/settings.py
 
 sed -i 's/https:\/\/lesspass.com/https:\/\/pass.qxzg.xyz/g' packages/lesspass-site/index.html
 sed -i 's/https:\/\/lesspass.com/https:\/\/pass.qxzg.xyz/g' packages/lesspass-site/gulpfile.js
